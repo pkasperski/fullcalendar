bc9e6866d504979b7b58c597b671927d2cbbc0aa
Dennis Koks <mail@denniskoks.com>
2012-06-21 15:24:25 +0200

    Some small style adjustments (removed block at the bottom of every event, ui-resizable in events only on hover). Also simplified getSkinCss to get all the colors from the event object itself

--- src/util.js
+++ src/util.js
@@ -307,30 +307,12 @@ function setDayID(cell, date) {
 	});
 }
 
-
 function getSkinCss(event, opt) {
-	var source = event.source || {};
-	var eventColor = event.color;
-	var sourceColor = source.color;
-	var optionColor = opt('eventColor');
-	var backgroundColor =
-		event.backgroundColor ||
-		eventColor ||
-		source.backgroundColor ||
-		sourceColor ||
-		opt('eventBackgroundColor') ||
-		optionColor;
-	var borderColor =
-		event.borderColor ||
-		eventColor ||
-		source.borderColor ||
-		sourceColor ||
-		opt('eventBorderColor') ||
-		optionColor;
-	var textColor =
-		event.textColor ||
-		source.textColor ||
-		opt('eventTextColor');
+    // Retrieve complete color-structure from the event it self only
+    var textColor = event.color;
+	var backgroundColor = event.backgroundColor || event.color;
+    var borderColor = event.borderColor || event.color;
+
 	var statements = [];
 	if (backgroundColor) {
 		statements.push('background-color:' + backgroundColor);
